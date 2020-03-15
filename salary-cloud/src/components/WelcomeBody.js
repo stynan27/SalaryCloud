311bf3119e43379d208b801cdbdbeba1ecd47126
shamoose <seamustynan16@gmail.com>
2020-03-15 00:55:55 -0400

    Created basic drop-down form for creating an account

--- salary-cloud/src/components/WelcomeBody.js
+++ salary-cloud/src/components/WelcomeBody.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import './WelcomeBody.css';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 function WelcomeBody() {
   return (
     <div className="WelcomeBody">
       <h1>Welcome to SalaryCloud</h1>
-      <button>Create Account</button>
+      
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Create Account
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu className="text-center">
+            <InputGroup className="mb-3">
+              <FormControl
+                input type="email"
+                placeholder="Email"
+                aria-label="Email"
+                aria-describedby="email-input"
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <FormControl
+                input type="password"
+                placeholder="Password"
+                aria-label="Password"
+                aria-describedby="password-input"
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <FormControl
+                input type="password"
+                placeholder="Confirm Password"
+                aria-label="Confirm Password"
+                aria-describedby="confirmation-input"
+              />
+            </InputGroup>
+
+            <Button variant="primary">
+              Submit
+            </Button>{' '}
+
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 }
