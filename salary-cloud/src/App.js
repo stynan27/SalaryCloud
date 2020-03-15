fd98a429eee7aa0efac0d8d2bbe0991ca37fbdea
yesuc <ycarter@colgate.edu>
2020-03-14 23:24:55 -0400

    added navbar and boostrap support

--- salary-cloud/src/App.js
+++ salary-cloud/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
 import WelcomeBody from './components/WelcomeBody';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar className="border-bottom border-success" bg="light" variant="light">
+        <Navbar.Brand className="mr-auto" href="/">
+          <h1> Salary Cloud </h1>
+        </Navbar.Brand>
+
+        <Nav className="ml-auto">
+          <Nav.Item className="mr-2">
+            <Nav.Link className="btn btn-success text-white" href="#" role="button"> Profile </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link className="btn btn-success text-white" href="#" role="button"> Login </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar>
+
       <WelcomeBody />
     </div>
   );
--- salary-cloud/src/index.js
+++ salary-cloud/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Insert main React App Component within root div
 ReactDOM.render(<App />, document.getElementById('root'));
-
