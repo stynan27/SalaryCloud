21cd2bea7af7f289a54bf4bd17209f11c4c7ea6f
shamoose <seamustynan16@gmail.com>
2020-03-16 22:31:38 -0400

    changed App functional component to a class component

--- salary-cloud/src/App.js
+++ salary-cloud/src/App.js
@@ -4,27 +4,37 @@ import WelcomeBody from './components/WelcomeBody';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar className="border-bottom border-success" bg="light" variant="light">
-        <Navbar.Brand className="mr-auto" href="/">
-          <h1> Salary Cloud </h1>
-        </Navbar.Brand>
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      componentPath: "",
+      loggedIn: false,
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Navbar className="border-bottom border-success" bg="light" variant="light">
+          <Navbar.Brand className="mr-auto" href="/">
+            <h1> Salary Cloud </h1>
+          </Navbar.Brand>
 
-        <Nav className="ml-auto">
-          <Nav.Item className="mr-2">
-            <Nav.Link className="btn btn-success text-white" href="#" role="button"> Profile </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link className="btn btn-success text-white" href="#" role="button"> Login </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Navbar>
+          <Nav className="ml-auto">
+            <Nav.Item className="mr-2">
+              <Nav.Link className="btn btn-success text-white" href="#" role="button"> Profile </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className="btn btn-success text-white" href="#" role="button"> Login </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
 
-      <WelcomeBody />
-    </div>
-  );
+        <WelcomeBody />
+      </div>
+    );
+  }
 }
 
 export default App;
