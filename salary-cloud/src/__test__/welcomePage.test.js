5d6f01ac9c7dd8addbfa905abdbee33480785a43
Yesu Carter <ycarter@colgate.edu>
2020-05-10 16:32:28 -0400

    Added tests assertion tests for welcome body dropdown display

--- salary-cloud/src/__test__/welcomePage.test.js
+++ salary-cloud/src/__test__/welcomePage.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import WelcomeBody from '../components/welcome/WelcomeBody';
 import DropDownForm from '../components/welcome/DropDownForm';
 import Dropdown from 'react-bootstrap/Dropdown';
+
+
 import renderer from 'react-test-renderer';
 import {render, fireEvent, screen} from '@testing-library/react'
 
@@ -9,7 +11,21 @@ import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme'
 import {shallow} from 'enzyme';
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
+
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+});
 
 describe ('Welcome Page', () => {
   it ('should render the usual html without crashing', () => {
@@ -25,24 +41,36 @@ describe ('Welcome Page', () => {
       expect(dropdown).toMatchSnapshot();
     });
 
-    it ('should render input groups that update onChange, but retain input', () => {
-      // const dropdown = shallow(<DropDownForm loggedIn={false} handleLogIn={() => {return true}}/>);
-      // var handleInputChange = (input) => { preventDefault: () => console.log('preventDefault')};
-      // console.log(dropdown.debug());
-      // const inputGroup = body.find('.input-group');
-      // expect(inputGroup).toHaveLength(3);
-      // expect(inputGroup[0].find('input').prop('type')).toEqual('email');
-      // expect(inputGroup[1].find('input').prop('type')).toEqual('password');
-      // expect(inputGroup[2].find('input').prop('type')).toEqual('password');
-      // // Check for Submit Button
-      // const buttons = body.find('button');
-      // expect(buttons).to.have.lengthOf(2);
-      // expect(buttons[1].text()).toEqual('Submit');
+    it ('should not display the Dropdown Form before the Create Account button is clicked', () => {
+      render(<WelcomeBody />);
+      // NOTE: Null == no element found
+      // Find Create Account Button
+      const dropdownButton = screen.queryByText(/create account/i);
+      expect(dropdownButton).not.toBeNull();
+      // Assert that dropdown inputs and submit button should not be present
+      expect(screen.queryByPlaceholderText(/email/i)).toBeNull();
+      expect(screen.queryByPlaceholderText(/password/i)).toBeNull();
+      expect(screen.queryByPlaceholderText(/confirm password/i)).toBeNull();
+      expect(screen.queryByText(/submit/i)).toBeNull();
     });
 
-    it ("should remove the dropdown on click", () => {
+    it ('should create a Dropdown Form when Create Account is clicked', () => {
       render(<WelcomeBody />);
-      expect(screen.queryByText('Create Account')).toBeInTheDocument();
+      const dropdownButton = screen.queryByText(/create account/i);
+      expect(dropdownButton).not.toBeNull();
+      // Click Button and assert that inputs and button are present
+      // NOTE: Will fail assertions if elements found != 1
+      fireEvent.click(dropdownButton);
+      const emailInput = screen.queryByPlaceholderText(/email/i);
+      expect(emailInput).not.toBeNull();
+      const passwordInput = screen.queryByPlaceholderText(/^password$/i);
+      expect(passwordInput).not.toBeNull();
+      // Match on placeholder containing 'confirm password' or 'password confirmation' strings
+      const passwordConfInput = screen.queryByPlaceholderText(/confirm password|password confirmation/i);
+      expect(passwordConfInput).not.toBeNull();
+      const submitButton = screen.queryByText(/submit/i);
+      expect(passwordConfInput).not.toBeNull();
+      console.log(screen.debug());
     });
 
   });
