e6d266df045a524e26e9ffec079ca77e2172900d
Yesu Carter <ycarter@colgate.edu>
2020-07-02 11:52:18 -0400

    Add Selected Housing Char. Request

--- salary-cloud/server/citysdk.js
+++ salary-cloud/server/citysdk.js
@@ -2,12 +2,13 @@ const census = require('citysdk');
 const fs = require('fs');
 const API_KEY = "57abfc64813428ef60094a8f010d67b1cd152b02";
 
+// https://api.census.gov/data/2018/acs/acs1/profile/variables.html
 
 const outString = fs.readFileSync('./county_codes_to_names.json','utf8');
 const code_to_name = JSON.parse(outString);
 
-
-let data = census({
+// HUEST (House Unit Estimates)
+const huest = {
   sourcePath: ["pep", "housing"],
   vintage: '2019',
   values: ["HUEST", "DATE_DESC", "COUNTY"],
@@ -16,12 +17,32 @@ let data = census({
     county: "*"
   },
   key: API_KEY
-}, (error, response) => {
+};
+
+// Selected Housing Unit Characteristics
+// https://data.census.gov/cedsci/table?q=rent&hidePreview=false&tid=ACSDP1Y2018.DP04&vintage=2018
+
+// DP04_0126E seems to be the total number of people surveyed.
+// it might be better to instead use the median rent for a given county
+// Median: DP04_0134E
+const huc = {
+  sourcePath: ["acs", "acs1", "profile"],
+  vintage: '2018',
+  values: ["NAME", "DP04_0134E"],
+  geoHierarchy: {
+    state: "36",
+    county: "*"
+  },
+  key: API_KEY
+}
+
+let data = census(huc, (error, response) => {
   if (error) {
     console.log(error);
     return;
   }
-  const target = matchByCountyNumber(63, response);
+  console.log(response);
+  const target = matchByCountyNumber(01, response);
   console.log(target);
 });
 
@@ -31,6 +52,9 @@ function matchByCountyNumber(fipsNum, json) {
   // County values in response have leading 0 if int < 100
   // Need to pad our target in that case
   if (fipsNum < 100) {
+    target = '0' + target;
+  }
+  if (fipsNum < 10) {
     target = '0' + target;
   }
   for (var i = 0; i < json.length; i++) {
