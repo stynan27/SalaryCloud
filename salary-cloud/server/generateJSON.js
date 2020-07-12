5fb382697ee260e63d5ceb960ee6e5edf419d057
Yesu Carter <ycarter@colgate.edu>
2020-07-12 15:52:24 -0400

    Added CleanUp Func for raw county code csv file

--- salary-cloud/server/generateJSON.js
+++ salary-cloud/server/generateJSON.js
@@ -5,11 +5,15 @@ const CSV_FILE = 'state-geocodes-v2018.csv';
 const KEY_COL = 2;
 const VAL_COL = 3;
 const SHOULD_SORT = false;
-let json = {};
+var json = {};
 
-function generateJSON() {
-  fs.exists(NEW_FILE, (exists) => {
-    fs.readFile(CSV_FILE, 'utf-8', (error, csvString) => {
+function generateJSON(csvFileName, newFileName) {
+  fs.exists(csvFileName, (exists) => {
+    if (!exists) {
+      console.log(`CSV File titled ${csvFileName} does not exist, cannot be read.`);
+      return;
+    }
+    fs.readFile(csvFileName, 'utf-8', (error, csvString) => {
       if (error) {
         console.log(error);
         return;
@@ -40,7 +44,7 @@ function generateJSON() {
       if (SHOULD_SORT) {
         // TODO: sort file
       } else {
-        fs.writeFile(NEW_FILE, JSON.stringify(json), (error, result) => {
+        fs.writeFile(newFileName, JSON.stringify(json), (error, result) => {
           if (error) {
             console.log(error);
             return;
@@ -51,4 +55,56 @@ function generateJSON() {
   });
 }
 
-generateJSON();
+const SPLIT_STEP = 6;
+
+/*
+  Creates a copy of a given CSV File where a given column is split on the first instance of a digit and a new line character is inserted at the beginning of the new column. This function assumes that the csv rows are not separated and will insert the new line character where the last alphabetical character is adjacent to the first digit every SPLIT_STEP columns.
+*/
+function cleanUp(csvFileName, newFileName) {
+  if (!newFileName.match(/\.csv$/)) {
+    newFileName += '.csv';
+  }
+
+  fs.exists(csvFileName, (exists) => {
+    if (!exists) {
+      console.log(`CSV File titled ${csvFileName} not found, cannot be read.`);
+      return;
+    }
+
+    fs.readFile(csvFileName, 'utf-8', (error, csvString) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      var cells = csvString.split(',');
+      var i = SPLIT_STEP;
+      while (i < cells.length) {
+        // Extract countyName and adjacent code
+        let target = cells[i];
+        target = target.replace(/\r/g, '');
+        let splitAt = target.search(/[0-9]/);
+        let countyName = target.slice(0, splitAt);
+        let newCode = target.slice(splitAt);
+        // Replace current index with countyName
+        cells[i] = countyName;
+        // Insert adjacent code in next index
+        // Also prepend a \n for readability
+        cells.splice(i+1, 0, "\n" + newCode);
+        i += SPLIT_STEP + 1;
+      }
+
+      fs.writeFile(newFileName, cells.toString(), (error, result) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+      });
+
+    });
+
+
+  });
+}
+
+cleanUp('all-geocodes-v2018.csv', 'county_codes_to_names.csv');
